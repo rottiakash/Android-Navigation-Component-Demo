f7ade03549470edb5f823d2d27bd2a1cd0d87b29
Akash <rottiakash@gmail.com>
2019-07-19 13:01:51 +0530

    Trying Kotlin Android Extensions

--- app/src/main/java/com/example/jetpackfirst/TimeTableFragment.kt
+++ app/src/main/java/com/example/jetpackfirst/TimeTableFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_time_table.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -23,8 +25,14 @@ class TimeTableFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_time_table, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toolsb.setOnClickListener {
+            findNavController().navigate(R.id.action_frag1_to_frag2)
+        }
     }
 
 
